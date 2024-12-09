5c30232a3fd5a85663020559545b3982de05a7c4
Mahoo12138 <mahoo12138@qq.com>
2024-12-09 14:17:51 +0800

    消息通信增加单向监听

--- src/utils/vscode.ts
+++ src/utils/vscode.ts
@@ -2,15 +2,24 @@ export const vscode = acquireVsCodeApi();
 
 export class VSCodeMessenger {
   private readonly listeners = new Map<string, (data: any) => void>();
+  private readonly persistentListeners = new Map<string, Array<(data: any) => void>>();
 
   constructor() {
     window.addEventListener('message', (event) => {
       const { type, data } = event.data;
       console.log('event', event);
+      
+      // 处理一次性监听器
       const listener = this.listeners.get(type);
       if (listener) {
         listener(data);
-        this.listeners.delete(type); // 自动清除处理器
+        this.listeners.delete(type);
+      }
+
+      // 处理持久监听器
+      const persistentListenerList = this.persistentListeners.get(type);
+      if (persistentListenerList) {
+        persistentListenerList.forEach(callback => callback(data));
       }
     });
   }
@@ -21,8 +30,26 @@ export class VSCodeMessenger {
       vscode.postMessage({ type, data: payload });
     });
   }
-}
 
+  // 添加持久监听器，不会自动清除
+  on(type: string, callback: (data: any) => void) {
+    const existingListeners = this.persistentListeners.get(type) || [];
+    existingListeners.push(callback);
+    this.persistentListeners.set(type, existingListeners);
+    
+    // 返回一个取消监听的方法
+    return () => {
+      const updatedListeners = (this.persistentListeners.get(type) || [])
+        .filter(cb => cb !== callback);
+      
+      if (updatedListeners.length > 0) {
+        this.persistentListeners.set(type, updatedListeners);
+      } else {
+        this.persistentListeners.delete(type);
+      }
+    };
+  }
+}
 
 
 // async function getPermissionFiles() {
