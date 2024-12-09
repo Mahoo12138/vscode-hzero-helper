353e26baac53a324de149b21835f9b7ef55940d2
Mahoo12138 <mahoo12138@qq.com>
2024-12-09 16:14:47 +0800

    优化消息通信逻辑

--- src/utils/vscode.ts
+++ src/utils/vscode.ts
@@ -7,8 +7,7 @@ export class VSCodeMessenger {
   constructor() {
     window.addEventListener('message', (event) => {
       const { type, data } = event.data;
-      console.log('event', event);
-      
+
       // 处理一次性监听器
       const listener = this.listeners.get(type);
       if (listener) {
@@ -25,10 +24,14 @@ export class VSCodeMessenger {
   }
 
   sendMessage<T>(type: string, payload?: any): Promise<T> {
-    return new Promise((resolve) => {
-      this.listeners.set(`${type}Response`, resolve);
-      vscode.postMessage({ type, data: payload });
+    return new Promise<T>((resolve) => {
+      this.listeners.set(`${type}_Response`, resolve);
+      vscode.postMessage({ type, data: payload, resp: true });
     });
+  }
+
+  postMessage(type: string, payload?: any) {
+    vscode.postMessage({ type, data: payload });
   }
 
   // 添加持久监听器，不会自动清除
@@ -36,12 +39,12 @@ export class VSCodeMessenger {
     const existingListeners = this.persistentListeners.get(type) || [];
     existingListeners.push(callback);
     this.persistentListeners.set(type, existingListeners);
-    
+
     // 返回一个取消监听的方法
     return () => {
       const updatedListeners = (this.persistentListeners.get(type) || [])
         .filter(cb => cb !== callback);
-      
+
       if (updatedListeners.length > 0) {
         this.persistentListeners.set(type, updatedListeners);
       } else {
